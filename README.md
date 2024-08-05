2a9fde17d66c4f8fb8a6c71d01c299b8c4d6f7b6
xjzh123 <37945610+xjzh123@users.noreply.github.com>
2024-08-05 22:00:19 +0800

    update README.md and bot.py

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # dotbot-neverland
 
-Describe your project here.
+A hack.chat bot framework which is simple, powerful, robust.
+
+## Documents
+
+WIP
+
+## Credits
+
+- foolishbird by light/await
+
+  This inspired me to start both making HC bots and learning Python.
+
+- [hvicorn](https://github.com/Hiyoteam/hvicorn) by [0x24a](https://github.com/0x24a)
+
+  This bot framework realized my dream of a "well typed" bot framework. It is very creative compared to previous bot frameworks, and dotbot-neverland references it heavily.
---
+++ test/async_handler.py
@@ -0,0 +1,32 @@
+import asyncio
+import os
+
+from dotbot_neverland import Bot, BotJoinEvent, ChatEvent, Context, WhisperEvent
+
+bot = Bot()
+
+
+@bot.on("onlineSet")
+async def hello(c: Context[BotJoinEvent]):
+    await c.bot.chat("Hello from dotbot-neverland!")
+
+
+@bot.on("chat", "whisper")
+async def ping(c: Context[ChatEvent | WhisperEvent]):
+    if c.event.text == ":ping":
+        await asyncio.sleep(1)
+        await c.reply("1")
+        await asyncio.sleep(2)
+        await c.reply("3")
+
+
+@bot.on("chat", "whisper")
+async def ping2(c: Context[ChatEvent | WhisperEvent]):
+    if c.event.text == ":ping":
+        await asyncio.sleep(2)
+        await c.reply("2")
+        await asyncio.sleep(2)
+        await c.reply("4")
+
+
+asyncio.run(bot.connect("lounge", "ping", os.getenv("HC_PWD"), bypass_dns=True))
