6632adb5dd8ed4f7dcb15e72bdb3be85b01d6e99
xjzh123 <37945610+xjzh123@users.noreply.github.com>
2024-08-06 13:18:26 +0800

    test: update users.py

--- test/users.py
+++ test/users.py
@@ -13,10 +13,15 @@ async def hello(c: Context[SelfJoinEvent]):
 
 @bot.on("chat", "whisper")
 async def ping(c: Context[ChatEvent | WhisperEvent]):
-    if c.event.text == ":users":
-        await c.reply(
-            "```py\n{}\n```".format(list(str(c.bot.nicks))), whisper_newline=True
-        )
+    match c.event.text.split():
+        case [":users"]:
+            await c.reply(
+                "```py\n{}\n```".format(str(list(c.bot.nicks))), whisper_newline=True
+            )
+        case [":hash", str() as n] if n in c.bot.users_dict:
+            await c.reply(c.bot.users_dict[n].hash)
+        case [":hash"]:
+            await c.reply(c.get_user().hash)
 
 
 asyncio.run(bot.connect("lounge", "users", os.getenv("HC_PWD"), bypass_dns=True))
